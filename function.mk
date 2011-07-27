e5fec23110e2acfbe7dde72dace9f47ef2e060b3
net_robber <net_robber@timectrl.net>
2011-07-27 13:50:08 +0400

    增加gcc编译过程内存占用控制:64M

--- function.mk
+++ function.mk
@@ -13,17 +13,17 @@ mkdir -p $(dir $(dir $2))
 @if [ `file $1|grep -oe '\<bzip2\>' || echo error` = 'bzip2' ]; \
 then \
    echo file type: bzip2; \
-   rm -fr $(dir $2)/`tar jtf $1|head -n 1` $2; \
-   tar -j -x -C $(dir $2) -f $1 && mv $(dir $2)/`tar jtf $1|head -n 1` $2; \
+   rm -fr $(dir $2)/`tar jtf $1 2>/dev/null |head -n 1` $2; \
+   tar -j -x -C $(dir $2) -f $1 && mv $(dir $2)/`tar jtf $1 2>/dev/null|head -n 1` $2; \
 elif [ `file $1|grep -oe '\<gzip\>' || echo error` = 'gzip' ]; \
 then \
    echo file type: gzip; \
-   rm -fr $(dir $2)/`tar ztf $1|head -n 1` $2 ;\
-   tar -z -x -C $(dir $2) -f $1 && mv $(dir $2)/`tar ztf $1|head -n 1` $2 ;\
+   rm -fr $(dir $2)/`tar ztf $1 2>/dev/null|head -n 1` $2 ;\
+   tar -z -x -C $(dir $2) -f $1 && mv $(dir $2)/`tar ztf $1 2>/dev/null|head -n 1` $2 ;\
 elif [ `file $1|tr [:upper:] [:lower:]|grep -oe '\<zip\>' || echo error` = 'zip' ]; \
 then \
    echo file type: zip; \
-   rm -fr $(dir $2)/`unzip -l $1 |head -n 4|tail -n 1|awk '{print $$4}'` $2 ; \
+   rm -fr $(dir $2)/`unzip -l $1 2>/dev/null|head -n 4|tail -n 1|awk '{print $$4}'` $2 ; \
    unzip -q -d $(dir $2) $1 && mv $(dir $2)/`unzip -l $1 |head -n 4|tail -n 1|awk '{print $$4}'` $2 ; \
 fi
 endef

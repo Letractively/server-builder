95182d98a01329373792d2b80f7cc9cd8d1ebef8
net_robber <net_robber@timectrl.net>
2011-08-30 12:19:06 +0800

    update src to php 5.3.8 nginx 1.0.6

--- url.mk
+++ url.mk
@@ -1,3 +1,3 @@
-url_php:='http://www.php.net/get/php-5.3.6.tar.bz2/from/this/mirror'
-url_nginx:='http://nginx.org/download/nginx-1.0.5.tar.gz'
+url_php:='http://www.php.net/get/php-5.3.8.tar.bz2/from/this/mirror'
+url_nginx:='http://nginx.org/download/nginx-1.0.6.tar.gz'
 url_mysql:='http://dev.mysql.com/get/Downloads/MySQL-5.5/mysql-5.5.15.tar.gz/from/http://mysql.he.net/'

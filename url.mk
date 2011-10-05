2d894ead80b3b74893e21573d8f532987ac98e24
root <root@lab.timectrl.net>
2011-10-05 08:51:17 +0400

    update source version

--- url.mk
+++ url.mk
@@ -1,3 +1,3 @@
 url_php:='http://www.php.net/get/php-5.3.8.tar.bz2/from/this/mirror'
-url_nginx:='http://nginx.org/download/nginx-1.0.6.tar.gz'
-url_mysql:='http://dev.mysql.com/get/Downloads/MySQL-5.5/mysql-5.5.15.tar.gz/from/http://mysql.he.net/'
+url_nginx:='http://nginx.org/download/nginx-1.0.8.tar.gz'
+url_mysql:='http://dev.mysql.com/get/Downloads/MySQL-5.5/mysql-5.5.16.tar.gz/from/http://mysql.he.net/'
